63d11dcf45df6a4f80066f08600fc54f325bc3ce
stackchow <stackchow@gmail.com>
2023-10-09 18:01:33 +0100

    Added validations

--- script.js
+++ script.js
@@ -39,23 +39,15 @@ function identifyCarrier() {
     const carrierResult = document.getElementById("carrierResult");
     const phoneNumber = phoneNumberInput.value;
 
-    //well i know some of this logic is fixed with enabling only the number keypad 
-    //but for cases where you cant restrict the users input its good to work on the logic
-    //plus it would be extra fun
-
     //todo: check if the formfield contains other characters asides digits
     //todo: if so remove them and then add the digits together
-    //todo: eg: (ds&klkn1.3jhs4#587ksjf *  oefisj498) would turn to (134587498)
-
+    
     //Filter the number inputted
     let filteredPhoneNo;
+    //todo: eg: (ds&klkn1.3jhs4#587ksjf *  oefisj498) would turn to (134587498)
     phoneNumber[0] == 0 ? filteredPhoneNo = phoneNumber.replaceAll(/\D/g, "") : filteredPhoneNo = "+" + phoneNumber.replaceAll(/\D/g, "");
-    console.log(filteredPhoneNo);
 
     //todo: and then we'd check if the number length is within the designated length(that is too short or too long)
-
-    //todo: Add validation - Use the pattern attribute of the HTML form field to restrict phone numbers to a certain carrier, 
-    //todo: e.g restrict to only Airtel such that entering an MTN/GLO number would be invalid
 
     //todo: Support +XYZ country codes (e.g +234 for Nigeria) - 
     //todo: Still detect the carrier even if the user prefixed the number with their +XYZ country code
@@ -66,36 +58,37 @@ function identifyCarrier() {
     const airtelPattern = /^(\+234|0)(802|902|701|808|708|812|901|907)\d{7}$/;
     const etisalatPattern = /^(\+234|0)(809|909|817|818|908)\d{7}$/;
 
-    // this is where the logos are stored
-    const gloLogo = document.querySelector('.glo-logo img');
-    const mtnLogo = document.querySelector('.mtn-logo img');
     const airtelLogo = document.querySelector('.airtel-logo img');
-    const eMobileLogo = document.querySelector('.mobile-logo img');
     const noCarrierLogo = document.querySelector('.nocarrier-logo img');
-
     const displayNetworkImg = document.getElementById('displayNetworkImg'); //Img element
     
+    //todo: Add validation - Use the pattern attribute of the HTML form field to restrict phone numbers to a certain carrier, 
+    //todo: e.g restrict to only Airtel such that entering an MTN/GLO number would be invalid
+    const errorMessage = document.getElementById('invalid');
+
     // checking for matched numbers
     if (mtnPattern.test(filteredPhoneNo)) {
         displayNetworkImg.src = "./assets/mtnlogo.png"; // Set the source (URL) of the image
-        carrierResult.textContent = "MTN";
-        // You can also display an MTN logo here
-        mtnLogo.style.display = 'block'
+
+        //Since only Airtel is Valid!
+        errorMessage.style.display = 'block';
+        carrierResult.textContent = "Error! Only Airtel is Valid!";
     } else if (gloPattern.test(filteredPhoneNo)) {
         displayNetworkImg.src = "./assets/Globacom-Limited-Logo.svg"; // Set the source (URL) of the image
-        carrierResult.textContent = "GLO";
-        // You can also display a GLO logo here
-        gloLogo.style.display = 'block'
+        
+        //Since only Airtel is Valid!
+        errorMessage.style.display = 'none';
+        carrierResult.textContent = "Error! Only Airtel is Valid!";
     } else if (airtelPattern.test(filteredPhoneNo)) {
-        displayNetworkImg.src = "./assets/mtnlogo.png"; // Set the source (URL) of the image
-        carrierResult.textContent = "Airtel";
-        // You can also display an Airtel logo here
-        airtelLogo.style.display = 'block'
+        displayNetworkImg.src = "./assets/Airtel_Networks_Limited-Logo.wine.svg"; // Set the source (URL) of the image
+        carrierResult.textContent = "Valid!";
+        airtelLogo.style.display = 'block'; // You can also display an Airtel logo here
     } else if (etisalatPattern.test(filteredPhoneNo)) {
         displayNetworkImg.src = "./assets/9mobile-Logo.svg"; // Set the source (URL) of the image
-        carrierResult.textContent = "9mobile";
-        // You can also display a 9mobile logo here
-        eMobileLogo.style.display = 'block'
+
+        //Since only Airtel is Valid!
+        errorMessage.style.display = 'none';
+        carrierResult.textContent = "Error! Only Airtel is Valid!";
     } else {
         carrierResult.textContent = "Carrier not recognized";
         noCarrierLogo.style.display = 'block';

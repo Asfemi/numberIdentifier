cf626d5bd39580598d4cb98d2b63a29c98262935
Asfemi <pearlcstall@gmail.com>
2023-10-15 16:36:00 +0100

    further updates

--- script.js
+++ script.js
@@ -24,31 +24,46 @@ function validatePhoneNumber(phoneNumber, selectedCarrier) {
 }
 
 
+function toggleSelect() {
+    // Get a reference to the select element
+    const selectElement = document.getElementById("carrierSelect");
+
+    // Toggle the display property
+    if (selectElement.style.display === "none") {
+        selectElement.style.display = "block";
+        selectElement.click();
+    } else {
+        selectElement.style.display = "none";
+    }
+}
+
 function handleFormSubmission(event) {
     event.preventDefault();
     const phoneNumberField = document.getElementById("phoneNumber");
     const carrierSelect = document.getElementById("carrierSelect");
     const selectedCarrier = carrierSelect.value;
     const phoneNumber = phoneNumberField.value;
-    
-    if (!/^[\d+]+$/.test(phoneNumber)) {
 
-        alert("Phone number should contain only digits.");
-    } else if (phoneNumber.length !== 14 ) { 
-        if ( phoneNumber.length !== 13){
-            if( phoneNumber.length !== 11){
-               alert("Phone number should be 11 digits long.");
+    let identifiedCarrier; // Initialize the identifiedCarrier variable
+
+    if (phoneNumber.length !== 14) {
+        if (phoneNumber.length !== 13) {
+            if (phoneNumber.length !== 11) {
+                alert("Phone number should be 11 digits long.");
             }
         }
-        
     } else if (!validatePhoneNumber(phoneNumber, selectedCarrier)) {
-       
+        identifiedCarrier = selectedCarrier; // Set identifiedCarrier to the selectedCarrier value
         alert(`Invalid phone number for ${selectedCarrier}. Please enter a valid number.`);
     } else {
-        
+        identifiedCarrier = selectedCarrier; // Set identifiedCarrier to the selectedCarrier value
         alert(`Valid phone number for ${selectedCarrier}. Proceeding with the submission.`);
     }
+
+    // Call the identifyCarrier function and pass the identifiedCarrier variable
+    identifyCarrier(identifiedCarrier);
 }
+
 
 
 
@@ -78,6 +93,35 @@ function updateCarrierIcon() {
 
 const carrierSelect = document.getElementById("carrierSelect");
 carrierSelect.addEventListener("change", updateCarrierIcon);
+
+function identifyCarrier(identifiedCarrier) {
+
+    // Update the carrier result div and logos
+    const carrierResultDiv = document.getElementById("carrierResult");
+    const carrierLogos = document.querySelectorAll(".fancy-bg > div");
+    const allCarriers = ["mtn", "glo", "9mobile", "airtel"]; // List of all carriers
+
+    if (allCarriers.includes(identifiedCarrier)) {
+        carrierResultDiv.textContent = identifiedCarrier.toUpperCase();
+        // Hide all logos
+        for (let logo of carrierLogos) {
+            logo.style.display = "none";
+        }
+        // Show the identified carrier's logo
+        const identifiedLogo = document.querySelector(`.${identifiedCarrier}-logo`);
+        if (identifiedLogo) {
+            identifiedLogo.style.display = "block";
+        }
+    } else {
+        carrierResultDiv.textContent = "Carrier not recognized";
+        // Show the default carrier logo
+        const defaultLogo = document.querySelector(".nocarrier-logo");
+        if (defaultLogo) {
+            defaultLogo.style.display = "block";
+        }
+    }
+}
+
 
 
 updateCarrierIcon();
